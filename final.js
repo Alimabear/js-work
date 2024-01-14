615ad8c2f21c879f4cd9be650b3a29cddbb52816
Alima <alimaaarshad20@gmail.com>
2024-01-14 13:28:34 +0500

    iss

---
+++ final.js
@@ -0,0 +1,38 @@
+
+// user input
+
+// function add() {
+//     let input = prompt("Enter numbers");
+  
+//     let numbers = input.split(",");
+  
+//     let sum = 0;
+  
+//     for (let i = 0; i < numbers.length; i++) {
+
+//       let num = parseFloat(numbers[i]);
+  
+//       if (!isNa(num)) {
+//         sum += num;
+//       } else {
+//         console.log(`Invalid input: ${numbers[i]} is not a valid number.`);
+//       }
+//     }
+  
+//     console.log(`Sum of the entered numbers: ${sum}`);
+//   }
+  
+//   add();
+
+
+// hard coded vales
+
+// var int1 = 12;
+// var int2 = 123;
+// sum = int1 + int2 ;
+// alert("the sum of" + int1 + "and" + int2 + "is " + sum);
+
+
+
+
+  
